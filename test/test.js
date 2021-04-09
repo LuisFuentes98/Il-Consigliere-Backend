8c506b7eaf60bedef75cab9c0f6dd3763b5f2c87
estebgonza29 <esteb.gonza29@gmail.com>
2021-04-09 13:30:55 -0600

    Pruebas func. 2

--- test/test.js
+++ test/test.js
@@ -1,8 +1,11 @@
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 var assert = require('assert');
-let server = require('../server');
+let UserRoutes = require('../server/routes/UserRoutes');
+let AttendantRouter = require('../server/routes/AttendantRouter');
 let should = chai.should();
+
+chai.use(chaiHttp);
 
 describe('Basic Mocha String Test', function () {
  it('should return number of charachters in a string', function () {
@@ -14,17 +17,38 @@ describe('Basic Mocha String Test', function () {
     });
 });
 
-describe('UserControler Test', function () {
-    describe('Prueba de envio de correo a asistentes a consejos', function(){
+describe('UserRoutes Test', function () {
+    describe('Prueba de envio de correo a usuarios nuevos', function(){
 
         it('devuelve status 200 si se envía el correo', function() {
-            let book = {
+            let data = {
                 correo: "tetox11@gmail.com",
                 permisos: "",
                 id_tipo_convocado: "2"
             }
-            chai.request(server)
-              .post('/usuario/enviar_link')
+            chai.request(UserRoutes)
+              .post('/enviar_link')
+              .send(data)
+              .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('json');                
+                done();
+              });
+        });
+    
+    });
+});
+
+describe('AttendantRouter Test', function () {
+    describe('Prueba de envio de correo a asistentes de consejos', function(){
+
+        it('devuelve status 200 si se envía el correo', function() {
+            let data = {
+                consecutivo: [],
+                convocados: [],                
+            }
+            chai.request(AttendantRouter)
+              .post('/')
               .send(data)
               .end((err, res) => {
                     res.should.have.status(200);
